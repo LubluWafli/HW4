b888ccfec16bfaf03b8c8379d24e87a78f9c6f39
Akame style <eiljo@mail.ru>
2025-04-25 03:01:50 +0300

    2

--- app/src/main/java/com/example/hw4/MainActivity.java
+++ app/src/main/java/com/example/hw4/MainActivity.java
@@ -1,36 +1,21 @@
 package com.example.hw4;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.hw4.databinding.ActivityMainBinding;
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-
-import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final String APP_PREFERENCES = "notes";
+    SharedPreferences mNotes;
     private RecyclerView recyclerView;
     private MyAdapter adapter;
     private List<String> data = new ArrayList<>(); // Изменим на изменяемый ArrayList
@@ -42,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        mNotes = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
 
         // Инициализация RecyclerView
         recyclerView = binding.recyclerView;
@@ -49,16 +35,27 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MyAdapter(data);
         recyclerView.setAdapter(adapter);
         
+        for (int i = 1; i < mNotes.getInt("itemNum",0) + 1; i++)
+        {
+            adapter.addItem(mNotes.getString(String.valueOf(i), "null"));
+        }
+        
         EditText textInput = binding.textInput;
         // Обработчик кнопки
-        Button addButton = binding.createNoteButton;
+        FloatingActionButton addButton = binding.createNoteButton;
         addButton.setOnClickListener(v -> {
             // Добавляем новый элемент
             String newItem = String.valueOf(textInput.getText());
             adapter.addItem(newItem);
+            
+            SharedPreferences.Editor editor = mNotes.edit();
+            editor.putString(String.valueOf(adapter.getItemCount()), newItem);
+            editor.putInt("itemNum", adapter.getItemCount());
+            editor.apply();
             textInput.setText("");
             // Прокручиваем к новому элементу
             recyclerView.smoothScrollToPosition(adapter.getItemCount() - 1);
         });
     }
+
 }
